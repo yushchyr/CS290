643664cb7c103adb4bbb929763b4d38b7e0425e3
Unknown <yushchyr@uregina.ca>
2018-02-08 22:28:31 -0600

    Strip and Redo
    One file multiple members.

--- ASS2/assignment2.py
+++ ASS2/assignment2.py
@@ -3,49 +3,41 @@ print("Liquid Resistor test")
 print("We will examine a new same for each new concentration of salt.")
 print("Make sure to start the first cycle before adding any salt")
 print("Please Begin Your Experiment by placing two Electrodes Into a Cup of a Distilled Water")
-
+print("")
 # Test scope and size settings
+
+import serial as s # Iport Serial 
+ser = s.Serial('/dev/ttyACM1') # Open Serial port
+print("Energia is connected at: " + ser.name) # Greating
+
 sampleTrialNumber = input("Enter number of Trials in this Test: ")
 sampleNumberPerTrial = input("Enetr number of sample's per each Trial: ")
+dataSetIndex = 1
+thefile = open('Data.txt', 'w')
+
+
 
 # Function that scan Serial
-def scan():
-    import serial as s # Iport Serial 
-    import time as t # Import Time
-
-    ser = s.Serial('/dev/ttyACM0') # Open Serial port
-    print("Energia is connected at: " + ser.name) # Greating
-    
-    dataSetIndaex = 1
+def scan(dataSetIndex):
     sampleIndex = 1
-    thefile = open('Data[1].txt', 'w')
-    for x in range(0, int(sampleNumberPerTrial)):
-        data = str(ser.readline().strip()) 
-        if data!='' and data!='/r' and data!='/n' and len(data) <= 3:
-            if(sampleIndex == 1 and dataSetIndaex == 1):
-                print("Trial One - Distilled water - NO salt")
-                input("Press Enter to begin!")
-                print("Data Set " + str(dataSetIndaex))
-                print("Sample# 1")
-                print(data)
-                thefile.write(str(data))
-                sampleIndex = 2
-            elif(sampleIndex >= sampleTrialNumber):
-                print("You are all done! Let's do some math!")
-                input("Please press Enter key...")
-            else:
-                sampleIndex += 1
-            print("Data Set " + str(dataSetIndaex))
-            print("Sample# " + str(sampleIndex))
-            print(data)
-            thefile.write(data)
-            t.sleep(1)
-            dataSetIndaex += 1
-    thefile.close()
-    ser.close()
-    dataSetIndaex += 1
-
+    thefile.write(str(dataSetIndex))
+    thefile.write(',')
+    dataSetIndex += 1
+    return int(dataSetIndex)
+    
 # Function Call
 for x in range(0,int(sampleTrialNumber)):
-    scan()
+    dataSetIndex = scan(dataSetIndex)
 
+thefile.close()
+### Error Bar
+
+#import matplotlib.pyplot as plt
+#x=[200,300, 400, 500, 600, 700, 800, 900]
+#y=[1.1, 1.5, 1.9, 2.8, 3.4, 3.5, 4.6, 5.4]
+#ey =[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,]
+#plt.errorbar(x,y,yerr=ey, fmt='0') 
+#plt.plot([0,1000], [0, 5.5], 'k-','lw=2')
+#plt.show
+
+###
